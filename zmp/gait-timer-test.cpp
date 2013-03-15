4bc4d558e64119e1286e4b35d8bbbc633acb764b
Saul Reynolds-Haertle <sreynoldshaertle@gmail.com>
2013-03-15 07:31:58 -0400

    change name of gait to gait-timer and refactor it and its tests/examples

--- zmp/gait-timer-test.cpp
+++ zmp/gait-timer-test.cpp
@@ -1,18 +1,72 @@
-#include "gait.h"
-#include <iostream>
+#include "gait-timer.h"
 
-using namespace Eigen;
 using namespace std;
+using namespace Eigen;
 
-const int stance_foot_table[4] = { 0, 1, 0, 1 };
-const int swing_foot_table[4] = { -1, -1, 1, 0 };
+
+///////////////////////////////////////////////////////////////
+// some prototypes for private helper functions
+
+// generates a stepping pattern
+std::vector<step_t> steps(int, bool);
+
+// compute step times from step pattern
+step_traj_t step_times(std::vector<step_t>, GaitTimer timer);
+
+
+
+int main() {
+
+	//////////////////////////////////////////////////////////////////////
+	// test gait timer
+	GaitTimer timer;
+	timer.startup_time = 1.0; // hold ZMP at center for 1s at start
+	timer.shutdown_time = 1.0; // hold ZMP at center for 1s at end
+	timer.single_support_time = 0.70; // .70s of single support each step
+	timer.double_support_time = 0.05; // .05s of double support each step
+
+	//timer.set_duty_single(0.55, 0.70);
+
+	std::cerr << timer << std::endl;
+
+	int n_steps = 8;
+
+	step_traj_t step_traj = step_times(steps(n_steps, true), timer);
+
+	//////////////////////////////////////////////////////////////////////
+	// print out, pipe to matlab/timing.txt and run matlab/zmp_plot.m
+	size_t eric_ticks = step_traj.stance.size();
+	EIGEN_V_VEC2D eric_zmpref = step_traj.zmpref;
+	std::vector<stance_t> eric_stance = step_traj.stance;
+
+	std::cout << "i x y stance" << std::endl;
+	for(size_t i=0; i < eric_ticks; i++) {
+		using namespace std;
+		using namespace Eigen;
+
+		Vector2d zmp = eric_zmpref[i];
+		stance_t stance = eric_stance[i];
+
+		cout << i << " "
+				<< zmp(0) << " "
+				<< zmp(1) << " "
+				<< stance << endl;
+	}
+}
+
+
+
+///////////////////////////////////////////////////////////////
+// implementations of private helper functions
+
 
 const stance_t next_stance_table[4] = {
-		SINGLE_LEFT,
-		SINGLE_RIGHT,
-		DOUBLE_RIGHT,
-		DOUBLE_LEFT
+  SINGLE_LEFT,
+  SINGLE_RIGHT,
+  DOUBLE_RIGHT,
+  DOUBLE_LEFT
 };
+
 
 vector<step_t> steps(int num_steps, bool firstleft) {
 	vector<step_t> step_seq;
@@ -59,7 +113,7 @@ vector<step_t> steps(int num_steps, bool firstleft) {
 	return step_seq;
 }
 
-step_traj_t step_times(vector<step_t> step_seq, step_timer_t timer) {
+step_traj_t step_times(vector<step_t> step_seq, GaitTimer timer) {
 	EIGEN_V_VEC2D zmpref; // b/c cannot dynamically resize matrixxd
 	vector<stance_t> stance;
 
