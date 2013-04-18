aa9e43c4c364c83fb017c775c827e83e60aa1d63
Arash Rouhani <rarash@gatech.edu>
2013-04-17 22:51:40 -0400

    Add more samples for opengl drawing

--- zmp/footprint-opengl.cpp
+++ zmp/footprint-opengl.cpp
@@ -10,16 +10,48 @@
 // Thanks too <http://www.dgp.toronto.edu/~ah/csc418/fall_2001/tut/square> for
 // showing me how to open an opengl context/window!
 
+std::vector<Footprint> footprints;
+
 void display (void){
   glClear( GL_COLOR_BUFFER_BIT);
   glColor3f(0.0, 1.0, 0.0);
   glBegin(GL_POLYGON);
-  drawFootprints(sampleFootprints(100));
+  drawFootprints(footprints);
   glEnd();
   glFlush();
 }
 
 int main(int argc, char **argv) {
+  double radius = 3;
+  double distance = 13;
+  double width = .2;
+  double max_step_length = .5;
+  double max_step_angle = M_PI / 6;
+
+  Footprint stance_foot = Footprint(0, width, 0, true);
+
+  footprints = walkCircle(radius,
+                          distance,
+                          width,
+                          max_step_length,
+                          max_step_angle,
+                          stance_foot);
+
+  footprints = turnInPlace(
+      2.0,
+      width,
+      max_step_length,
+      stance_foot);
+
+  Footprint end_foot = Footprint(2-width, 2, M_PI/2, true);
+
+  footprints = walkTo(
+      width,
+      max_step_length,
+      max_step_angle,
+      stance_foot,
+      end_foot);
+
   glutInit(&argc, argv);
   glutInitDisplayMode ( GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
 
