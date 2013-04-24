6dabe8f8e595d7f2fd822d9cd58dd629b763949f
pvieira3 <pete.vieira@gatech.edu>
2013-04-24 16:25:20 -0400

    added comments

--- src/ZmpPreview.h
+++ src/ZmpPreview.h
@@ -29,20 +29,27 @@ public:
   double T, h, g;
   size_t nl;
 
+  // drift, control and output matrices for state equation
   Eigen::Matrix3d A;
   Eigen::Vector3d B;
   Eigen::RowVector3d C;
 
+  // drift and control matrices for augmented state equation
   Eigen::Matrix4d AA;
   Eigen::Vector4d BB;
+
+  // LQR terms
   double RR;
   Eigen::Matrix4d QQ;
   
+  // Unknown in Riccati equation
   Eigen::Matrix4d PP;
 
+  // integral gain and state feedback gain
   double Ks;
   Eigen::RowVector3d Kx;
 
+  // Preview controller gain term
   Eigen::RowVectorXd G;
 
   bool debug;
@@ -66,6 +73,7 @@ public:
     nl = numlookahead;
     g = gaccel;
 
+    // state equation terms
     A << 
       1, T, T*T/2, 
       0, 1, T,
@@ -75,6 +83,7 @@ public:
 
     C << 1, 0, -h/g;
 
+    // augmented state equation terms
     AA(0,0) = 1;
     AA.block<1,3>(0,1) = C*A;
     AA.block<3,1>(1,0).setZero();
@@ -83,6 +92,7 @@ public:
     BB(0,0) = C*B;
     BB.block<3,1>(1,0) = B;
 
+    // LQR terms in eq. J = x^T*Q*x + u^T*R*u
     QQ << 
       Qe, 0, 0, 0,
       0, Qdpos, 0, 0, 
@@ -92,6 +102,7 @@ public:
     RR = R;
     
 
+    // solve the Riccati equation iteratively for PP
     PP.setIdentity();
     bool converged = false;
     
@@ -115,22 +126,20 @@ public:
     }
 
     
-
+    // construct K in the state feedback equation u = -Kx
     double SS = 1.0/(RR + BB.transpose() * PP * BB);
-    
     Eigen::RowVector4d KK = SS * BB.transpose()*PP*AA;
-    Ks = KK(0,0);
-    Kx = KK.block<1,3>(0,1);
+    Ks = KK(0,0); // intergral gain
+    Kx = KK.block<1,3>(0,1); // state feedback gain
     
+    // new state feedback drift term (A matrix)
     Eigen::Matrix4d Ac = AA - BB*KK;
     Eigen::Vector4d CC;
     CC << 1, 0, 0, 0;
 
+    // construct preview action gain terms
     Eigen::Vector4d XX = -Ac.transpose() * PP * CC;
-
-
     G = Eigen::RowVectorXd(nl);
-
     G(0) = -Ks;
     for (size_t i=1; i<nl; ++i) {
       G(i) = (SS * BB.transpose() * XX);
@@ -155,28 +164,35 @@ public:
     
   }
 
+  /*
+   * @function: update(Eigen::Vector3d& X,
+   *                   double& e,
+   *                   const Eigen::ArrayBase<Derived>& zmpref))
+   * @brief: updates the zmp error and zmp
+   */
   template <class Derived> double update(Eigen::Vector3d& X,
 					 double& e,
 					 const Eigen::ArrayBase<Derived>& zmpref) const {
 
-
-
-
     size_t nref = zmpref.rows();
     assert(nref);
 
+    // controller: u_k = -K_i*e(k) - K_x*x(k)
     double u = -Ks*e - Kx*X;
     double zs = 0;
 
+    // add on preview action gain using future reference: -sum(G_p(j)*zmp_ref(k+j)
     for (size_t i=0; i<nl; ++i) {
       double zr = zmpref(i < nref ? i : nref-1);
       u -= zr * G(i);
       zs +=  zr;
     }
-				       
+ 
+    // final state equation
     X = A*X + B*u;
     double zmp = C*X;
 
+    // zmp error
     e += zmp - zmpref(0);
 
     if (debug) {
@@ -189,10 +205,8 @@ public:
     }
 
     return zmp;
-    
   }
 	     
-
 };
 
 #endif
