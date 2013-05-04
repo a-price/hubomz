02a0a79e0a612de4e8e36028d26e9c7ffbd04926
Pete <pete.vieira@gatech.edu>
2013-05-04 17:47:15 -0400

    rewrote patch script

--- patch.sh
+++ patch.sh
@@ -1,11 +1,30 @@
-cd ..
-# clone openHubo repo if it doesn't exist
-if [ ! -d "openHubo" ]; then
-    git clone https://github.com/daslrobotics/openHubo.git
+# This script will
+#   -patch the huboplus OpenRAVE model
+#   -create a build diretory
+#   -run cmake to create a makefile
+#   -compile
+
+# create the patch if it doesn't already exit
+if [ ! -d "huboplus" ]; then
+    cd ..
+    # clone openHubo repo if it doesn't exist
+    if [ ! -d "openHubo" ]; then
+        git clone https://github.com/daslrobotics/openHubo.git
+    fi
+
+    # patch the huboplus OpenRAVE model
+    cd openHubo
+    patch -p0 < ../hubomz/huboplus.patch
+    cd ../hubomz
+    ln -s ../openHubo/robots/huboplus
 fi
 
-# clone patch the huboplus OpenRAVE model
-cd openHubo
-patch -p0 < ../hubomz/huboplus.patch
-cd ../hubomz
-ln -s ../openHubo/robots/huboplus
+# create build directory if not there and cmake
+if [ ! -d "build" ]; then
+    mkdir build
+fi
+
+cd build
+cmake .. -DCMAKE-BUILD_TYPE=Release
+make
+
